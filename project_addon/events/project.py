9b0c0588d01976d08f683428ac90164cdd67e0a1
beingeek <furqan.790@outlook.com>
2023-06-25 12:20:54 +0500

    feat: override project dashboard to show timesheet record connections

--- project_addon/events/project.py
+++ project_addon/events/project.py
@@ -22,3 +22,19 @@ def make_timesheet(doc):
 	
 	return timesheet.name
 
+
+def get_data():
+	return {
+		"heatmap": True,
+		"heatmap_message": _("This is based on the Time Sheets created against this project"),
+		"fieldname": "project",
+		"transactions": [
+			{
+				"label": _("Project"),
+				"items": ["Task", "Timesheet", "Time Sheet Record", "Issue", "Project Update"],
+			},
+			{"label": _("Material"), "items": ["Material Request", "BOM", "Stock Entry"]},
+			{"label": _("Sales"), "items": ["Sales Order", "Delivery Note", "Sales Invoice"]},
+			{"label": _("Purchase"), "items": ["Purchase Order", "Purchase Receipt", "Purchase Invoice"]},
+		],
+	}
